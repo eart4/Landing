41ce4e40ffcc12a3fc94f36ea5d21235605fddbc
eart4 <metrika987@gmail.com>
2023-03-02 23:10:30 +0300

    create: created adaptive

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ input.addEventListener('focus', function () {
 input.addEventListener('blur', function () {
     form.classList.remove('form--active');
 })
+
+
+document.querySelector('.menu-btn').addEventListener('click', function(
+
+    ){
+        document.querySelector('.menu-btn .first').classList.toggle('active')
+        document.querySelector('.menu-btn .second').classList.toggle('active')
+        document.querySelector('.menu-btn .third').classList.toggle('active')
+        document.querySelector('.navigation__list-burger').classList.toggle('active')
+        
+        
+    })
+    
+    
+    document.querySelector('.navigation__list-burger').addEventListener('click', function(
+    
+        ){
+            document.querySelector('.menu-btn .first').classList.toggle('active')
+            document.querySelector('.menu-btn .second').classList.toggle('active')
+            document.querySelector('.menu-btn .third').classList.toggle('active')
+            document.querySelector('nav').classList.toggle('active')
+            
+            
+        })
